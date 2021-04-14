194a84effdd5a62dbf14ea0cf2706028f70978aa
Carlos <zataraincarlos@hotmail.com>
2021-04-13 20:35:34 -0700

    First commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert anything: ");
+        String s = input.nextLine();
+        System.out.println(s);
+    }
+}
